5b92f37c2f9af044b4319f868cf39b16c086b7f6
Christopher Pappas <cpappas@oreilly.com>
2016-11-05 19:50:48 -0400

    Adding extract_xml_and_clean function and relevant tests

--- scraper/scraper.py
+++ scraper/scraper.py
@@ -47,7 +47,7 @@ def extract_xml_and_clean(response):
     # Consider also dropping all \n \t \r
 
     xml_clean = etree.fromstring(xml_dirty)
-    print(type(xml_clean))
+
     return xml_clean
 
 def get_data(url):
@@ -59,6 +59,98 @@ def get_data(url):
 
     return extract_xml_and_clean(scrape(url))
 
+def convert_xml_to_dict(xml):
+    '''
+    Take clean xml object and take relevant fields
+    and put it into a dictionary
+
+    Helper functions here would be overkill.
+    :param xml:
+    :return:
+    '''
+    datadict = {}
+
+    # Splitting up data extraction from dictionary insertion in case
+    # there needs to be any formatting done
+    extraction = xml.find('credit').text
+    datadict['credit'] = extraction
+
+    extraction = xml.find('suggested_pickup').text
+    datadict['suggested_pickup'] = extraction
+
+    extraction = xml.find('suggested_pickup_period').text
+    datadict['suggested_pickup_period'] = extraction
+
+    extraction = xml.find('location').text
+    datadict['location'] = extraction
+
+    extraction = xml.find('station_id').text
+    datadict['station_id'] = extraction
+
+    extraction = xml.find('latitude').text
+    datadict['latitude'] = extraction
+
+    extraction = xml.find('longitude').text
+    datadict['longitude'] = extraction
+
+    extraction = xml.find('observation_time').text
+    datadict['observation_time'] = extraction
+
+    extraction = xml.find('observation_time_rfc822').text
+    datadict['observation_time_rfc822'] = extraction
+
+    extraction = xml.find('weather').text
+    datadict['weather'] = extraction
+
+    extraction = xml.find('temp_f').text
+    datadict['temp_f'] = extraction
+
+    extraction = xml.find('temp_c').text
+    datadict['temp_c'] = extraction
+
+    extraction = xml.find('relative_humidity').text
+    datadict['relative_humidity'] = extraction
+
+    extraction = xml.find('wind_dir').text
+    datadict['wind_dir'] = extraction
+
+    extraction = xml.find('wind_degrees').text
+    datadict['wind_degrees'] = extraction
+
+    extraction = xml.find('wind_mph').text
+    datadict['wind_mph'] = extraction
+
+    extraction = xml.find('wind_gust_mph').text
+    datadict['wind_gust_mph'] = extraction
+
+    extraction = xml.find('wind_kt').text
+    datadict['wind_kt'] = extraction
+
+    extraction = xml.find('wind_gust_kt').text
+    datadict['wind_gust_kt'] = extraction
+
+    extraction = xml.find('pressure_mb').text
+    datadict['pressure_mb'] = extraction
+
+    extraction = xml.find('pressure_in').text
+    datadict['pressure_in'] = extraction
+
+    extraction = xml.find('dewpoint_f').text
+    datadict['dewpoint_f'] = extraction
+
+    extraction = xml.find('dewpoint_c').text
+    datadict['dewpoint_c'] = extraction
+
+    extraction = xml.find('windchill_f').text
+    datadict['windchill_f'] = extraction
+
+    extraction = xml.find('windchill_c').text
+    datadict['windchill_c'] = extraction
+
+    extraction = xml.find('visibility_mi').text
+    datadict['visibility_mi'] = extraction
+
+    return datadict
 
 
 if __name__ == "__main__":
--- scraper/tests/test_scraper.py
+++ scraper/tests/test_scraper.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 # encoding: utf-8
 
-
+import copy
 from lxml import etree
 from nose.tools import *
 import requests
@@ -11,9 +11,10 @@ import os
 
 from scraper import scraper
 
-class TestScraper:
+class TestScraper(object):
 
-    cassette_library_dir = os.path.join(os.path.dirname(__file__), 'files')
+    this_dir = os.path.dirname(__file__)
+    cassette_library_dir = os.path.join(this_dir, 'files')
 
     def __init__(self):
 
@@ -23,6 +24,8 @@ class TestScraper:
 
         self.response200 = self._run_200_get()
         self.response404 = self._run_404_get()
+
+        self.clean_xml = self._create_clean_xml()
 
     @vcr.use_cassette(os.path.join(cassette_library_dir, '200_get.yaml'))
     def _run_200_get(self):
@@ -51,6 +54,21 @@ class TestScraper:
         r = requests.get(self.url404)
 
         return r
+
+    def _create_clean_xml(self):
+        '''
+        Make some clean xml that our methods can work with
+        :return:
+        '''
+
+        path_to_file = os.path.join(
+            os.path.dirname(__file__),
+            'files/example_xml_data.xml'
+            )
+
+        with open(path_to_file, 'r') as handle:
+            xml = etree.parse(handle)
+            return xml
 
     @vcr.use_cassette(os.path.join(cassette_library_dir, '200_get.yaml'))
     def test_scrape(self):
@@ -93,3 +111,46 @@ class TestScraper:
         xml = scraper.extract_xml_and_clean(response)
 
         assert(isinstance(xml, etree._Element))
+
+    def test_convert_xml_to_dict(self):
+        '''
+        Takes XML and puts it in a dict so it can be used with a DB
+        :return:
+        '''
+
+        xml = copy.copy(self.clean_xml)
+        d = scraper.convert_xml_to_dict(xml)
+
+        # assert that all keys necessary for model are in dict
+        model_fields = [
+            'credit',
+            'suggested_pickup',
+            'suggested_pickup_period',
+            'location',
+            'station_id',
+            'latitude',
+            'longitude',
+            'observation_time',
+            'observation_time_rfc822',
+            'weather',
+            'temp_f',
+            'temp_c',
+            'relative_humidity',
+            'wind_dir',
+            'wind_degrees',
+            'wind_mph',
+            'wind_gust_mph',
+            'wind_kt',
+            'wind_gust_kt',
+            'pressure_mb',
+            'pressure_in',
+            'dewpoint_f',
+            'dewpoint_c',
+            'windchill_f',
+            'windchill_c',
+            'visibility_mi',
+        ]
+
+        for field in model_fields:
+            assert field in d
+
