d66960ee7d67a3c97b936588c7bf63c8e53beefb
Christopher Pappas <cpappas@oreilly.com>
2016-11-05 16:10:29 -0400

    Cleaning up testing paths for vcrpy

--- scraper/tests/test_scraper.py
+++ scraper/tests/test_scraper.py
@@ -7,11 +7,16 @@ from nose.tools import *
 import requests
 import vcr
 
+import os
+
 from scraper import scraper
 
 class TestScraper:
 
+    cassette_library_dir = os.path.join(os.path.dirname(__file__), 'files')
+
     def __init__(self):
+
         self.good_url = "http://w1.weather.gov/xml/current_obs/KBOS.xml"
         self.bad_url = "ahtdwtp://w1.weather.gov/xml/current_obs/KBOS.xml"
         self.url404 = "http://w1.weather.gov/christopappas_is_the_man"
@@ -19,7 +24,7 @@ class TestScraper:
         self.response200 = self._run_200_get()
         self.response404 = self._run_404_get()
 
-    @vcr.use_cassette('files/vcr_cassettes/200_get.yaml')
+    @vcr.use_cassette(os.path.join(cassette_library_dir, '200_get.yaml'))
     def _run_200_get(self):
         '''
         Helper script to create a good get response object.
@@ -33,7 +38,7 @@ class TestScraper:
 
         return r
 
-    @vcr.use_cassette('files/vcr_cassettes/404_get.yaml')
+    @vcr.use_cassette(os.path.join(cassette_library_dir, '404_get.yaml'))
     def _run_404_get(self):
         '''
         Helper script to create a good get response object.
@@ -47,7 +52,7 @@ class TestScraper:
 
         return r
 
-    @vcr.use_cassette('files/vcr_cassettes/200_get.yaml')
+    @vcr.use_cassette(os.path.join(cassette_library_dir, '200_get.yaml'))
     def test_scrape(self):
         '''
         Scrape should return requests object
@@ -58,7 +63,7 @@ class TestScraper:
 
         assert(isinstance(response, requests.models.Response))
 
-    @vcr.use_cassette('files/vcr_cassettes/404_get.yaml')
+    @vcr.use_cassette(os.path.join(cassette_library_dir, '404_get.yaml'))
     def test_scrape_non200(self):
         '''
         Scrape should throw exception on non 200 response
