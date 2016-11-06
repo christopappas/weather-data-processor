f5cc8080fdf8c28cee11b83eabb296f0b8b0e981
Christopher Pappas <cpappas@oreilly.com>
2016-11-06 01:03:47 -0400

    Moving over django app materials into delivery subdirectory

--- manage.py
+++ manage.py
@@ -2,8 +2,9 @@
 import os
 import sys
 
+
 if __name__ == "__main__":
-    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "weather-data-processor.settings")
+    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "delivery.settings")
     try:
         from django.core.management import execute_from_command_line
     except ImportError:
